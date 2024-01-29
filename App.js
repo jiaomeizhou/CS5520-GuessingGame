361a1902b43a457338b68a6bbf9ad4b81a89ce07
jiaomeizhou <zhoujiaomei@outlook.com>
2024-01-29 01:28:47 -0800

    enable navigate to the start screen from final screen

--- App.js
+++ App.js
@@ -10,11 +10,20 @@ export default function App() {
   const [number, setNumber] = useState('');
   const [isGameStarted, setIsGameStarted] = useState(false);
   const [isGameOver, setIsGameOver] = useState(false);
+  const [numberToGuess, setNumberToGuess] = useState(Math.floor(Math.random() * 10) + 1020); // [1020, 1029]
+  const [isWin, setIsWin] = useState(false);
+  const [attempts, setAttempts] = useState(2); // [0, 2]
 
   function receiveData(name, number) {
     console.log(name, number);
     setName(name);
     setNumber(number);
+  }
+
+  function updateNumberToGuessHandler() {
+    if (attempts === 0 || isGameOver) {
+      setNumberToGuess(Math.floor(Math.random() * 10) + 1020);
+    }
   }
 
   function startGameHandler(name, number) {
@@ -24,8 +33,21 @@ export default function App() {
     console.log('Game started');
   }
 
+  function continueGameHandler() {
+    setIsGameStarted(false);
+    setIsGameOver(false);
+    setNumber('');
+    setAttempts(2);
+  }
+
   function gameOverHandler() {
     setIsGameOver(true);
+    setIsGameStarted(false);
+  }
+
+  function restartGameHandler() {
+    setIsGameStarted(false);
+    setIsGameOver(false);
   }
 
   if (!isGameStarted && !isGameOver) {
@@ -40,7 +62,7 @@ export default function App() {
   if (isGameStarted && !isGameOver) {
     return (
       <View style={styles.container}>
-        <GameScreen onStartGame={startGameHandler} onFinish={gameOverHandler}/>
+        <GameScreen receiveData={receiveData} onStartGame={startGameHandler} onFinish={gameOverHandler} onContinue={continueGameHandler} />
         <StatusBar style="auto" />
       </View>
     );
@@ -48,7 +70,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <FinalScreen />
+      <FinalScreen onRestart={restartGameHandler} />
       <StatusBar style="auto" />
     </View>
   );
