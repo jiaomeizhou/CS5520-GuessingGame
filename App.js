004defc0aeb7fc85453187cd81603fc43c7f8023
jiaomeizhou <zhoujiaomei@outlook.com>
2024-01-28 23:38:26 -0800

    add conditional rendering for three screens

--- App.js
+++ App.js
@@ -1,13 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import StartScreen from './components/screens/startScreen';
+import GameScreen from './components/screens/gameScreen';
+import FinalScreen from './components/screens/finalScreen';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [isGameStarted, setIsGameStarted] = useState(false);
+  const [isGameOver, setIsGameOver] = useState(false);
+
+  if (!isGameStarted && !isGameOver) {
+    return (
+      <View style={styles.container}>
+        <StartScreen />
+        <StatusBar style="auto" />
+      </View>
+    ); 
+  }
+
+  if (isGameStarted && !isGameOver) {
+    return (
+      <View style={styles.container}>
+        <GameScreen />
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <FinalScreen />
       <StatusBar style="auto" />
     </View>
   );
+
 }
 
 const styles = StyleSheet.create({
